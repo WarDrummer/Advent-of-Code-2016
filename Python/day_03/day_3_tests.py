0dc096f5d21e857f994493655ba7aaf115aa4258
Jason Pike <jpike@atlasrfid.com>
2017-01-30 12:00:10 -0600

    removed unused test file

--- Python/day_03/day_3_tests.py
+++
@@ -1,7 +0,0 @@
-# import unittest
-#
-#
-# class TriangleValidatorTests(unittest.TestCase):
-#     def test_should_(self):
-#         key_pad = KeyPad.create_standard_keypad()
-#         self.assertEqual(key_pad.get_last_pressed_button(), "5")
