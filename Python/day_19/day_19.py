b03ac7180087c275768f06f5ea13ac82221c93db
Jason Pike <jpike@atlasrfid.com>
2017-02-10 22:07:11 -0600

    completed day 19, part 1

---
+++ Python/day_19/day_19.py
@@ -0,0 +1,25 @@
+
+class Node:
+    def __init__(self, elf_id):
+        self.elf_id = elf_id
+        self.next = None
+
+
+def create_circle(number_of_elves):
+    head = next_node = Node(1)
+    for i in range(2, number_of_elves + 1):
+        next_node.next = Node(i)
+        next_node = next_node.next
+    next_node.next = head
+    return head
+
+
+def get_winner_win_stealing_left(circle_of_elves):
+    while circle_of_elves.elf_id != circle_of_elves.next.elf_id:
+        circle_of_elves.next = circle_of_elves.next.next
+        circle_of_elves = circle_of_elves.next
+    return circle_of_elves.elf_id
+
+
+elves = create_circle(3004953)
+print(get_winner_win_stealing_left(elves))
