de124ac09f6c26b312d57b686bc20e1631ede8a9
Jason Pike <jpike@atlasrfid.com>
2017-03-05 09:23:11 -0600

    added tools projects to save typing

---
+++ CSharp/Tools/Tools/LineByLine.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace Tools
+{
+    public class LineByLine
+    {
+        public static IEnumerable<string> GetLines(string filename)
+        {
+            using (var file = new StreamReader("input.txt"))
+            {
+                string line;
+                while ((line = file.ReadLine()) != null)
+                {
+                    yield return line;
+                }
+            }
+        }
+    }
+}
