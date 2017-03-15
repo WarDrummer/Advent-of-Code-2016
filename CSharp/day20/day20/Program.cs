4013cd525f89e3ce9045a097467f559e5dd631dc
Jason Pike <jpike@atlasrfid.com>
2017-03-14 22:28:46 -0500

    day 20 parts 1 & 2

---
+++ CSharp/day20/day20/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Tools;
+
+namespace day20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ranges = new List<Range>();
+            foreach (var line in LineByLine.GetLines("input.txt"))
+            {
+                var split = line.Trim().Split('-');
+                ranges.Add(new Range(uint.Parse(split[0]), uint.Parse(split[1])));
+            }
+            ranges = ranges.OrderBy(r => r.Low).ToList();
+            var count = 0;
+            do
+            { 
+                count = ranges.Count;
+                for (var i = 0; i < ranges.Count - 1; i++)
+                {
+                    if (ranges[i].CanCombine(ranges[i + 1]))
+                    {
+                        ranges[i] = ranges[i].CombineWith(ranges[i + 1]);
+                        ranges.RemoveAt(i + 1);
+                    }
+                }
+            } while (count != ranges.Count);
+
+            Console.WriteLine((ranges[0].Low > 0) ? 0 : ranges[0].High + 1);
+
+            uint totalValid = 0;
+            for (var i = 0; i < ranges.Count - 1; i++)
+            {
+                totalValid += ranges[i + 1].Low - ranges[i].High - 1;
+            }
+
+            Console.WriteLine(totalValid);
+            Console.ReadKey();
+        }
+    }
+
+    class Range
+    {
+        public uint Low { get; set; }
+        public uint High { get; set; }
+
+        public Range(uint low, uint high)
+        {
+            Low = low;
+            High = high;
+        }
+
+        public bool CanCombine(Range range)
+        {
+            if (High + 1 == range.Low)
+            {
+                return true;
+            }
+
+            if (range.Low > Low && range.Low < High)
+            {
+                return true;
+            }
+
+            if (range.High > Low && range.High < High)
+            {
+                return true;
+            }
+
+
+            return false;
+        }
+
+        public Range CombineWith(Range range)
+        {
+            return new Range(
+                Math.Min(Low, range.Low), 
+                Math.Max(High, range.High));
+        }
+    }
+}
--- CSharp/day20/day20/Properties/AssemblyInfo.cs
+++ CSharp/day20/day20/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Tools")]
+[assembly: AssemblyTitle("day20")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Tools")]
+[assembly: AssemblyProduct("day20")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -19,7 +19,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("9d1eae6f-6d22-4e5b-94be-1663d997bc86")]
+[assembly: Guid("4863bbed-799e-4814-98ea-a62e231f4999")]
 
 // Version information for an assembly consists of the following four values:
 //
