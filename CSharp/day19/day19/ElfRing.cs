277037784903cb624d2540256a6581f2d5e23576
Jason Pike <jpike@atlasrfid.com>
2017-03-20 23:01:03 -0500

    completed day 19 parts 1 and 2 in c#

---
+++ CSharp/day19/day19/ElfRing.cs
@@ -0,0 +1,58 @@
+﻿namespace day19
+{
+    public class ElfRing
+    {
+        public static int Part1(int numberOfElves)
+        {
+            var elf = BuildElfRing(numberOfElves);
+            while (elf != elf.NextElf)
+            {
+                elf.NextElf = elf.NextElf.NextElf;
+                elf = elf.NextElf;
+            }
+            return elf.Id;
+        }
+
+        public static int Part2(int numberOfElves)
+        {
+            var elfCount = numberOfElves;
+            var elf = BuildElfRing(elfCount);
+
+            var firstRemoval = (elfCount / 2) - 1;
+            var elfPriorToRemoval = elf;
+
+            for (var i = 0; i < firstRemoval; i++)
+                elfPriorToRemoval = elfPriorToRemoval.NextElf;
+
+            if (elfCount % 2 == 1) // odd
+            {
+                elfPriorToRemoval.NextElf = elfPriorToRemoval.NextElf.NextElf;
+                elfPriorToRemoval = elfPriorToRemoval.NextElf;
+                elfCount--;
+            }
+
+            while (elfCount > 2)
+            {
+                elfPriorToRemoval.NextElf = elfPriorToRemoval.NextElf.NextElf;
+                elfPriorToRemoval.NextElf = elfPriorToRemoval.NextElf.NextElf;
+                elfPriorToRemoval = elfPriorToRemoval.NextElf;
+                elfCount -= 2;
+            }
+            
+            return elfPriorToRemoval.Id;
+        }
+
+        private static Elf BuildElfRing(int numberOfElves)
+        {
+            var firstElf = new Elf(1);
+            var currentElf = firstElf;
+            for (var i = 2; i <= numberOfElves; i++)
+            {
+                currentElf.NextElf = new Elf(i);
+                currentElf = currentElf.NextElf;
+            }
+            currentElf.NextElf = firstElf;
+            return firstElf;
+        }
+    }
+}
