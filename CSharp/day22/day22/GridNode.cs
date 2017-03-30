ac9d6ec8113de7fe51025bb0db1fa3fb06898388
Jason Pike <jpike@atlasrfid.com>
2017-03-29 20:19:26 -0500

    day 22 part 1 completed

---
+++ CSharp/day22/day22/GridNode.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace day22
+{
+    public class GridNode
+    {
+        public int X { get; private set; }
+        public int Y { get; private set; }
+        public int Used { get; private set; }
+        public int Available { get; private set; }
+
+        public static GridNode Create(string nodeInfo)
+        {
+            //Filesystem              Size  Used  Avail  Use%
+            // /dev/grid/node-x0-y0     93T   67T    26T   72%
+            var parts = nodeInfo.Split(new char[0], StringSplitOptions.RemoveEmptyEntries);
+            var fileSystem = parts[0].Split('-');
+
+            return new GridNode()
+            {
+                X = int.Parse(fileSystem[1].Substring(1, fileSystem[1].Length - 1)),
+                Y = int.Parse(fileSystem[2].Substring(1, fileSystem[2].Length - 1)),
+                Used = int.Parse(parts[2].Remove(parts[2].Length - 1)),
+                Available = int.Parse(parts[3].Remove(parts[3].Length - 1)),
+            };
+        }
+
+        public IEnumerable<GridNode> ValidAdjacentNodes(GridNode[][] nodes)
+        {
+
+            return null;
+        }
+
+        public int UniqueIdentifier()
+        {
+            return X + (Y << 16);
+        }
+
+        public bool MakesViablePair(GridNode b)
+        {
+            var a = this;
+            //    Node A is not empty(its Used is not zero).
+            //    Nodes A and B are not the same node.
+            //    The data on node A(its Used) would fit on node B(its Avail).
+            return a.Used != 0 && a.UniqueIdentifier() != b.UniqueIdentifier() && b.Available >= a.Used;
+        }
+    }
+}
---
+++ CSharp/day22/day22/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Tools;
+
+namespace day22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nodes = new GridNode[39][]; // 39 x 25
+            for (var i = 0; i < nodes.Length; i++)
+            {
+                nodes[i] = new GridNode[25];
+            }
+
+            foreach (var line in LineByLine.GetLines("input.txt"))
+            {
+                if (line.StartsWith("/dev"))
+                {
+                    var node = GridNode.Create(line);
+                    nodes[node.X][node.Y] = node;
+                }
+            }
+
+            Part1(nodes);
+            Console.ReadKey();
+        }
+
+        private static void Part1(GridNode[][] nodes)
+        {
+            var viablePairCount = 0;
+            var xLength = nodes.Length;
+            var yLength = nodes[0].Length;
+
+            for (var x1 = 0; x1 < xLength; x1++)
+            {
+                for (var y1 = 0; y1 < yLength; y1++)
+                {
+                    for (var x2 = 0; x2 < xLength; x2++)
+                    {
+                        for (var y2 = 0; y2 < yLength; y2++)
+                        {
+                            if (nodes[x1][y1].MakesViablePair(nodes[x2][y2]))
+                                viablePairCount++;
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine(viablePairCount);
+        }
+    }
+}
